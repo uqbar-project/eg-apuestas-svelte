dd07ddac2ad5a9e16d160ee06d05ca0b869b7e43
Fernando Dodino <fernando.dodino@gmail.com>
2024-11-25 22:54:00 -0300

    Using POJO vs. js object with behavior

--- src/apuestasPage.spec.ts
+++ src/apuestasPage.spec.ts
@@ -1,7 +1,7 @@
-import { describe, it, expect } from 'vitest'
+// import { describe, it, expect } from 'vitest'
 
-describe('Apuestas page', () => {
-	it('debe fallar si se ingresa un importe negativo', () => {
+// describe('Apuestas page', () => {
+// 	it('debe fallar si se ingresa un importe negativo', () => {
 		
-	})
-})
+// 	})
+// })
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,77 +1,68 @@
 <script lang='ts'>
   import './styles.css'
-	import { Apuesta, DOCENA, PLENO } from '$lib/apuesta'
-	import dayjs from 'dayjs'
-	import type { Resultado } from '$lib/resultado'
-	import Validador from '$lib/Validador.svelte'
+
+  import { Apuesta, DOCENA, PLENO } from '$lib/apuesta'
+  import Validador from '$lib/Validador.svelte'
+  import dayjs from 'dayjs'
 
   const formatearFecha = (fecha: Date) => dayjs(fecha).format('YYYY-MM-DD')
   
   let fechaApuesta = $state(formatearFecha(new Date()))
-  let monto = $state(0)
-  let tipoApuesta = $state(PLENO)
-  let valorApostado: any = $state(undefined)
-  let apuesta: Apuesta = $state(new Apuesta())
-
-  const updateApuesta = () => {
-    apuesta = new Apuesta()
-    apuesta.fecha = dayjs(fechaApuesta).toDate()
-    apuesta.monto = monto
-    apuesta.tipoApuesta = tipoApuesta
-    apuesta.valorApostado = valorApostado
-  }
-
-  let resultado: Resultado | null = $state(null)
-  let valoresAApostar = $derived(tipoApuesta?.valoresAApostar ?? [])
+  let apuestaPojo: any = $state({
+    monto: 0,
+    fecha: new Date(),
+    tipoApuesta: PLENO,
+    valorApostado: 1,
+  })
+  let apuestaModel: Apuesta = $state(new Apuesta())
 
   const fechaMinimaApuesta = formatearFecha(new Date())
   const tiposApuesta = [PLENO, DOCENA]
   const apostar = () => {
-    updateApuesta()
-    apuesta.apostar()
-    resultado = apuesta.resultado
+    apuestaModel = Object.assign(new Apuesta(), apuestaPojo)
+    apuestaModel.apostar()
   }
 </script>
 
 <form class="form">
   <div class="row">
     <label for="fecha" class="label">Fecha</label>
-    <input type="date" data-testid="fechaApuesta" bind:value={fechaApuesta} name="fechaApuesta" placeholder="Fecha de apuesta" min={fechaMinimaApuesta}>
-    <Validador elemento={apuesta} atributo='fecha'></Validador>
+    <input type="date" onchange={() => { apuestaPojo.fecha = dayjs(fechaApuesta).toDate() }} data-testid="fechaApuesta" bind:value={fechaApuesta} name="fechaApuesta" placeholder="Fecha de apuesta" min={fechaMinimaApuesta}>
+    <Validador elemento={apuestaModel} atributo='fecha'></Validador>
   </div>
   <div class="row">
     <label for="monto">Monto</label>
-    <input type="number" data-testid="monto" name="monto" class="numeric" bind:value={monto}
+    <input type="number" data-testid="monto" name="monto" class="numeric" bind:value={apuestaPojo.monto}
       placeholder="Monto en $" required={true}>
-    <Validador elemento={apuesta} atributo='monto'></Validador>
+    <Validador elemento={apuestaModel} atributo='monto'></Validador>
   </div>
   <div class="row">
     <label for="tipoApuesta">Tipo de Apuesta</label>
-    <select data-testid="tipoApuesta" name="tipoApuesta" class="form-control" bind:value={tipoApuesta}
-      required={true} onchange={() => { valorApostado = undefined}}>
+    <select data-testid="tipoApuesta" name="tipoApuesta" class="form-control" bind:value={apuestaPojo.tipoApuesta}
+      required={true} onchange={() => { apuestaPojo.valorApostado = ''}}>
       {#each tiposApuesta as tipo}
         <option value={tipo}>{tipo.descripcion}</option>
       {/each}
     </select>
-    <Validador elemento={apuesta} atributo="'tipoApuesta'"></Validador>
+    <Validador elemento={apuestaModel} atributo="'tipoApuesta'"></Validador>
   </div>
   <div class="row">
     <label for="apuesta">Qué apostás</label>
-    <select name="apuesta" data-testid="apuesta" class="form-control" bind:value={valorApostado}
-      disabled={!tipoApuesta?.valoresAApostar} required={true}>
-      {#each valoresAApostar as valor}
+    <select name="apuesta" data-testid="apuesta" class="form-control" bind:value={apuestaPojo.valorApostado}
+      disabled={!apuestaPojo.tipoApuesta?.valoresAApostar} required={true}>
+      {#each apuestaPojo.tipoApuesta?.valoresAApostar ?? [] as valor}
           <option value={valor}>{valor}</option>
       {/each}
     </select>
-    <Validador elemento={apuesta} atributo="'valorAApostar'"></Validador>
+    <Validador elemento={apuestaModel} atributo="'valorAApostar'"></Validador>
   </div>
   <div class="botonera">
     <button class="btn-primary" data-testid="btnApuesta" onclick={apostar} type="submit">Apostar</button>
   </div>
   <div class="row">
-      {#if (resultado)}
+      {#if (apuestaModel.resultado)}
         <div class="message" data-testid="resultado">
-          {resultado.valor()}
+          {apuestaModel.resultado.valor()}
         </div>
       {/if}
   </div>
