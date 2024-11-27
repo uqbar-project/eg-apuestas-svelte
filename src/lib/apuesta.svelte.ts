70e7aee3b6aac650d0dfe4e71a7f0a49b6b7ba77
Fernando Dodino <fernando.dodino@gmail.com>
2024-11-27 20:28:00 -0300

    WIP README

--- src/lib/apuesta.svelte.ts
+++ src/lib/apuesta.svelte.ts
@@ -101,7 +101,6 @@ export class Apuesta {
 	apostar() {
 		this.resultado = null
 		this.validarApuesta()
-		console.info('errores', this.errors)
 		if (this.errors.length > 0) return
 		const numeroGanador = this.obtenerNumeroGanador()
 		console.info('salió el ', numeroGanador, this.valorApostado)
