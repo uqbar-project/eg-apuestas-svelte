a0ba6b46b04d9db3841d928060324f16ffd6112f
fdodino <fernando.dodino@gmail.com>
2025-09-02 20:53:05 -0300

    add typing

--- src/lib/Validador.svelte
+++ src/lib/Validador.svelte
@@ -1,13 +1,23 @@
 <script lang="ts">
-	import './validador.css'
+  import './validador.css'
 
-	let { elemento, atributo } = $props()
+  type ViewModel = {
+    hasErrors: (_: string) => boolean
+    errorsFrom: (_: string) => string
+  }
+
+  type ValidatorProps = {
+    elemento: ViewModel
+    atributo: string
+  }
+
+  let { elemento, atributo }: ValidatorProps = $props()
 </script>
 
 {#if elemento.hasErrors(atributo)}
-	<div class="validation-row">
-		<div data-testid={`errorMessage-${atributo}`} class="validation">
-			{elemento.errorsFrom(atributo)}
-		</div>
-	</div>
+  <div class="validation-row">
+    <div data-testid={`errorMessage-${atributo}`} class="validation">
+      {elemento.errorsFrom(atributo)}
+    </div>
+  </div>
 {/if}
