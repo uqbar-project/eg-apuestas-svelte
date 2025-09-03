9da91f650aca1e0f24362a65297ebf3c6d56ee82
fdodino <fernando.dodino@gmail.com>
2025-09-02 21:02:28 -0300

    Non reactive

--- src/lib/apuesta.svelte.ts
+++ src/lib/apuesta.svelte.ts
@@ -56,12 +56,12 @@ export class ValidationMessage {
 }
 
 export class Apuesta {
-  fecha: Date | null = $state(null)
-  monto = $state(0)
-  tipoApuesta: TipoApuesta | null = $state(null)
-  valorApostado: number | string | null = $state(null)
-  resultado: Resultado | null = $state(null)
-  errors: ValidationMessage[] = $state([])
+  fecha: Date | null = null
+  monto = 0
+  tipoApuesta: TipoApuesta | null = null
+  valorApostado: number | string | null = null
+  resultado: Resultado | null = null
+  errors: ValidationMessage[] = []
 
   hasErrors(field: string): boolean {
     return this.errors.some((_) => _.field == field)
